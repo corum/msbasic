c7d5d49564a3e7787d6b51593d649a39ffcfff22
Eric Badger <ebadger@BADGERBOOK>
2023-06-22 18:29:48 -0700

    optimize the interpreter, add a couple of commands

--- dos.s
+++ dos.s
@@ -22,48 +22,52 @@ process_command:
     lda #$00
     sta dos_command, x
 
-    ; match with an existing command
-    jsr match_command
-    .byte "ECHO",0
-    cpx #$4
-    beq cmd_echo
-
-    jsr match_command
-    .byte "EXIT",0
-    cpx #$4
-    beq cmd_exit
-
-    jsr match_command
-    .byte "DIR",0
-    cpx #$3
-    beq cmd_dir
+; match with an existing command
 
     jsr match_command
     .byte "CD",0
-    cpx #$2
-    beq cmd_chdir
+    bcs @match_cls
+    jmp cmd_chdir
 
-    jsr display_message
-    .byte 10, 13, "Unknown Command", 10, 13, 0
-    bra newprompt
+@match_cls:
+    jsr match_command
+    .byte "CLS",0
+    bcs @match_dir
+    jmp cmd_cls
 
-cmd_echo:
-    jsr print_crlf
+@match_dir:
+    jsr match_command
+    .byte "DIR",0
+    bcs @match_echo
+    jmp cmd_dir
 
-@echoloop:
-    inx
-    lda dos_command, X
-    beq newprompt
-    jsr display_char
-    bra @echoloop
+@match_echo:
+    jsr match_command
+    .byte "ECHO",0
+    bcs @match_exit
+    jmp cmd_echo
 
-cmd_exit:
-    jsr display_message
-    .byte 10, 13, "Goodbye!", 10, 13, 0
+@match_exit:
+    jsr match_command
+    .byte "EXIT",0
+    bcs @match_quit
+    jmp cmd_exit
+
+@match_quit:
+    jsr match_command
+    .byte "QUIT",0
+    bcs @match_woz
+    jmp cmd_exit
+
+@match_woz:
+    jsr match_command
+    .byte "WOZ",0
+    bcs @unknown
     jmp WOZMON
 
-cmd_dir:
-    jsr fat32_dir
+@unknown:
+    jsr display_message
+    .byte 10, 13, "Unknown Command", 10, 13, 0
     jmp newprompt
 
 cmd_chdir:
@@ -84,27 +88,59 @@ cmd_chdir:
     jsr fat32_opendirent
     jmp newprompt
 
+; ************************************************************
+; COMMANDS
+
+cmd_cls:
+    jsr _cls
+    jmp newprompt
+
+cmd_dir:
+    jsr fat32_dir
+    jmp newprompt
+
+cmd_echo:
+    jsr print_crlf
+
+@echoloop:
+    inx
+    lda dos_command, X
+    beq @newprompt
+    jsr display_char
+    bra @echoloop
+@newprompt:
+    jmp newprompt
+
+cmd_exit:
+    jsr display_message
+    .byte 10, 13, "Goodbye!", 10, 13, 0
+    jmp WOZMON
+
+;****************************************************************************
+; STRING COMPARISON
 ; x contains the # of matching chars
+; if carry is cleared, it matches, if carry is set, it doesn't match
 match_command:
+    clc 
     ldx #0
     ldy	#0
     pla
     sta	MSG_ADDR_LOW
     pla
-    sta	MSG_ADDR_HIGH          ; get return address off the stack
+    sta	MSG_ADDR_HIGH       ; get return address off the stack
     bne	@increturn
 
 @nextchar:
-    lda	(MSG_ADDR_LOW),Y		; next message character    
-    beq	@pushreturnaddr		; done?	yes, exit
+    lda	(MSG_ADDR_LOW),Y	; next message character    
+    beq	@pushreturnaddr		; null terminator?	yes, exit
+    bcs @increturn          ; previous char didn't match - skip checking
     jsr	match_char
-    bne @increturn          ; doesn't match, exit
     inx
-
-@increturn:					; next address
+    
+@increturn:					; next byte
     inc	MSG_ADDR_LOW
     bne	@nextchar
-    inc	MSG_ADDR_HIGH	   	    ; fix MSB of next address
+    inc	MSG_ADDR_HIGH	   	; carry
     bne	@nextchar
 
 @pushreturnaddr:
@@ -115,12 +151,14 @@ match_command:
     rts
 
 
-    ; A contains the char to match
-    ; X contains the index into the string
+; A contains the char to match
+; X contains the index into the string
+; carry bit set if it doesn't match
 match_char:
     cmp dos_command, x
-    bne @nomatch
-    lda #0
-@nomatch:
+    clc
+    beq @matches
+    sec                      ; carry flag means doesn't match
+@matches:
     rts
 
